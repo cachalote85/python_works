031954b0bdc6034f5a6ac57f1fa01666b827f86a
Carlos I. Rosas M <cirosasm@gmail.com>
2022-03-26 21:54:18 -0300

    Update birthday.py

--- birthday.py
+++ birthday.py
@@ -1,5 +1,6 @@
-age = 23
-# notar que la edad esta casteada a string.
-message = 'Happy ' + str(age) + 'rd birthday' 
 
+
+# notar que la edad esta casteada a string.
+age = 23
+message = 'Happy ' + str(age) + 'rd birthday' 
 print (message)
